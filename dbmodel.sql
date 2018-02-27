1e3d56ec3fcdfcd3f040ec64ef8a3ef16dbc5228
arraymirez <36830590+arraymirez@users.noreply.github.com>
2018-02-26 21:59:46 -0800

    Modificacion modelo
    Se modifico el modelo de la bd con las respectivas tablas. Se agrego tabla transacciones y se agrego el balance del afiliado.

--- dbmodel.sql
+++ dbmodel.sql
@@ -4,12 +4,9 @@ USE Wn_db
 
 
 
-/*Balance corresponde al dinero del usuario*/
 CREATE TABLE Balance(
 bal_id INT primary key identity,
 bal_usuario VARCHAR(200), 
-bal_tran_id int
-)
 /*Direcciones del cliente o usuario*/
 CREATE TABLE Direcciones(
 dir_id INT PRIMARY KEY IDENTITY,
@@ -20,19 +17,18 @@ dir_cp VARCHAR(100),
 dir_municipio VARCHAR(100),
 dir_estado VARCHAR(100),
 dir_pais VARCHAR(100)
-
 )
 
 
 /*Clientes, los usuarios de la app*/
 CREATE TABLE Clientes(
-cl_id INT PRIMARY KEY IDENTITY,
-cl_nombre  VARCHAR(100),
-cl_aparterno VARCHAR(100),
-cl_amaterno VARCHAR(100),
-cl_direccion_id int foreign key(cl_direccion_id) references Direcciones(dir_id),
-cl_telefono VARCHAR(50),
-
+cli_id INT PRIMARY KEY IDENTITY,
+cli_nombre  VARCHAR(100),
+cli_aparterno VARCHAR(100),
+cli_amaterno VARCHAR(100),
+cli_direccion_id int foreign key(cli_direccion_id) references Direcciones(dir_id),
+cli_telefono VARCHAR(50),
+cli_email VARCHAR(70)
 )
 
 
@@ -44,6 +40,7 @@ afi_aparterno VARCHAR(100),
 afi_amaterno VARCHAR(100),
 afi_direccion_id int foreign key(afi_direccion_id) references Direcciones(dir_id),
 afi_telefono VARCHAR(50),
+afi_email VARCHAR(70)
 )
 
 /*Usuarios, credenciales de afiliados y usuarios*/
@@ -79,3 +76,19 @@ svc_ofi_id  INT FOREIGN KEY (svc_ofi_id) REFERENCES Oficios(ofi_id)
 
 
 
+/*Tabla de balance de los afiliados*/
+CREATE TABLE Transacciones(
+tran_id int primary key identity,
+tran_cli_id int foreign key (tran_cli_id) REFERENCES Clientes(cli_id) NULL,
+tran_afi_id int foreign key (tran_afi_id) REFERENCES Afiliados(afi_id) NULL,
+tran_descripcion VARCHAR(250),
+tran_monto money,
+tran_comision DECIMAL(4,2),
+tran_iva DECIMAL(4,2),
+tran_bloqueada bit
+)
+
+CREATE TABLE BALANCE(
+bal_afi_if int FOREIGN KEY(bal_afi_if) REFERENCES Afiliados(afi_id),
+bal_actual money
+)
